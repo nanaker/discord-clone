18c74255d7d5d90d12ba2140baa65df0b82d079d
nawal <nawal.kerkar@imadrassa.com>
2021-02-28 15:52:03 +0100

    add redux structure

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAKpwgc67ePNVC8IqaiGvSAonsB5xYII3I",
+  authDomain: "discord-clone-72cf6.firebaseapp.com",
+  projectId: "discord-clone-72cf6",
+  storageBucket: "discord-clone-72cf6.appspot.com",
+  messagingSenderId: "895733720231",
+  appId: "1:895733720231:web:ce372aca78630dfa596782",
+  measurementId: "G-BRS8JSFNQ8",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = firebaseApp.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
