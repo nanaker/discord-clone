6b74ef56200617d20b97a725c96b53650db49a27
nawal <nawal.kerkar@imadrassa.com>
2021-05-19 16:22:13 +0100

    add edit channel name

--- src/Components/SidebarChannel.js
+++ src/Components/SidebarChannel.js
@@ -1,10 +1,49 @@
-import React from "react";
+import { Tooltip } from "@material-ui/core";
+import { EditOutlined } from "@material-ui/icons";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setChannelInfo } from "../features/appSlice";
+import SaveIcon from "@material-ui/icons/Save";
+import Input from "@material-ui/core/Input";
+import db from "../config/firebase";
 
 const SidebarChannel = ({ id, channelName }) => {
   const dispatch = useDispatch();
-  return (
+  const [edit, setEdit] = useState(false);
+  const [value, setValue] = useState(channelName);
+  const editChannelName = async (e) => {
+    if (!e) var e = window.event;
+    e.cancelBubble = true;
+    if (e.stopPropagation) e.stopPropagation();
+    console.log(`save object`);
+    const channelRef = db.collection("channels").doc(id);
+    await channelRef.set({
+      channelName: value,
+    });
+    try {
+      await db.runTransaction(async (t) => {
+        const doc = await t.get(channelRef);
+        const newPopulation = doc.data().population + 1;
+        t.update(channelRef, { population: newPopulation });
+      });
+
+      console.log("Transaction success!");
+    } catch (e) {
+      console.log("Transaction failure:", e);
+    }
+  };
+  return edit ? (
+    <div className="sidebarChannel">
+      <Input
+        value={value}
+        onChange={(val) => setValue(val.currentTarget.value)}
+      />
+      :
+      <Tooltip title="Save">
+        <SaveIcon onClick={(e) => editChannelName(e)} />
+      </Tooltip>
+    </div>
+  ) : (
     <div
       onClick={() => dispatch(setChannelInfo({ channelId: id, channelName }))}
       className="sidebarChannel"
@@ -13,6 +52,17 @@ const SidebarChannel = ({ id, channelName }) => {
         <span className="sidebarChannel__hash">#</span>
         {channelName}
       </h4>
+      <Tooltip title="Edit">
+        <EditOutlined
+          onClick={(e) => {
+            if (!e) var e = window.event;
+            e.cancelBubble = true;
+            if (e.stopPropagation) e.stopPropagation();
+            console.log(`edit object`);
+            setEdit(true);
+          }}
+        />
+      </Tooltip>
     </div>
   );
 };
--- src/screens/App.js
+++ src/screens/App.js
@@ -4,20 +4,39 @@ import Chat from "../Components/Chat";
 import { selectUser } from "../features/userSlice";
 import { useSelector } from "react-redux";
 import Login from "../Components/Login";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function App() {
   const user = useSelector(selectUser);
+  const theme = createMuiTheme();
+  console.log(`theme`, theme);
+  theme.palette.primary = {
+    light: "#757ce8",
+    main: "#808080",
+    dark: "#808080",
+    contrastText: "#ffffff",
+  };
+  theme.palette.secondary = {
+    light: "#ff7961",
+    main: "#f44336",
+    dark: "#ba000d",
+    contrastText: "#ffffff",
+  };
+  theme.palette.text.primary = "rgb(128,128,128)";
+
   return (
-    <div className="app">
-      {user ? (
-        <>
-          <Sidebar />
-          <Chat />
-        </>
-      ) : (
-        <Login />
-      )}
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="app">
+        {user ? (
+          <>
+            <Sidebar />
+            <Chat />
+          </>
+        ) : (
+          <Login />
+        )}
+      </div>
+    </MuiThemeProvider>
   );
 }
 
